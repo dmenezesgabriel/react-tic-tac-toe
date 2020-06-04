741fdc890c9e7f33d090b0af99c3aec0d3fb7562
Gabriel Dias Menezes <dmenezes.gabriel@gmail.com>
2020-06-04 12:20:20 -0300

    Updates

---
+++ precache-manifest.3b5df4c747abe016cdc28c68e41754db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c979f6cc807d459a707249c58dfc1190",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6072ab64769e289fb183",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "7781336385eb55116ab5",
+    "url": "/react-tic-tac-toe/static/js/2.a8b385fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.a8b385fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6072ab64769e289fb183",
+    "url": "/react-tic-tac-toe/static/js/main.a970abbe.chunk.js"
+  },
+  {
+    "revision": "4eca8d306c2d5f6566de",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.d2cea36d.js"
+  }
+]);
